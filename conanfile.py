4b93521e5eeb4e4aa598fab419c553992a794016
bincrafters-user <bincrafters@gmail.com>
2019-09-13 01:52:15 +0000

    Update Conan conventions [build=outdated]
    Automatically created by bincrafters-conventions 0.10.0

--- conanfile.py
+++ conanfile.py
@@ -15,17 +15,17 @@ class RapiXMLConan(ConanFile):
     license = ("BSL-1.0", "MIT")
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
-    source_subfolder = "source_subfolder"
+    _source_subfolder = "source_subfolder"
     no_copy_source = True
 
     def source(self):
         source_url = "https://cfhcable.dl.sourceforge.net/project/rapidxml/rapidxml/rapidxml%20"
         tools.get("{0}{1}/{2}-{3}.zip".format(source_url, self.version, self.name, self.version))
-        os.rename(self.name + "-" + self.version, self.source_subfolder)
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
 
     def package(self):
-        self.copy(pattern="license.txt", dst="licenses", src=self.source_subfolder)
-        self.copy(pattern="*.hpp", dst="include", src=self.source_subfolder)
+        self.copy(pattern="license.txt", dst="licenses", src=self._source_subfolder)
+        self.copy(pattern="*.hpp", dst="include", src=self._source_subfolder)
 
     def package_id(self):
         self.info.header_only()
